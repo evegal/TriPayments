327e53c14eb9fafa80fc5f958f5ede8480f78c4e
Demetrios Kontizas <dkontizas@gmail.com>
2015-02-10 16:36:30 -0800

    No longer needed

--- app/features/src/recurringpayments/recurringpaymentsCtrl.js
+++
@@ -1,8 +0,0 @@
-app.controller('recurringpaymentsCtrl', function($scope,$http,$state,baseUrl,$rootScope,Notify) {
-
-  ///////////////////
-  // NOTIFY NEW USER
-  ///////////////////
-  console.log('!!!!!  recurringpaymentsCtrl');
-
-}); // end recurringpaymentsCtrl
