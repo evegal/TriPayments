3a30c6b689b516e717de21d2a2b2a0948342edfc
Demetrios Kontizas <dkontizas@gmail.com>
2015-04-07 13:30:53 -0700

    TPG-376 initial checkin for VT fix

--- app/features/src/vterminal/vterminalCtrl.js
+++ app/features/src/vterminal/vterminalCtrl.js
@@ -1,13 +1,46 @@
 app.controller('vterminalCtrl', function($rootScope,$scope,$http,$timeout,$state,$timeout, baseUrl) {
 
-$scope.authType = 'Charge';
-$scope.charge_form.currency = $scope.currencies[0].Name;
+    //REFERENCE DETAILS UNIQUE IDENTIFIER
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    //BIND TO INPUT FIELD
+    $scope.chargeRefnumber = uuid;
 
-console.log('test');
-console.log($scope.charge_form.currency);
+    // LOAD CURRENCIES - IN CASE USER HARD REFRESHES IN THIS PAGE
+    if($scope.currencies == undefined){
+        $http.get(baseUrl + 'currencies').success(function(data) {
+            $scope.currencies = data;
+            $scope.chargeCurrency = $scope.currencies[0].Id;
+        });
+    }
 
-$scope.auth_form = {};
 
+    $scope.chargeFname = 'Brian';
+    $scope.chargeLname = 'Phillips';
+    //$scope.chargeMidGroup = 
+    $scope.chargeAmount = 1;
+    $scope.chargeCcNumber = 487093009704848;
+    $scope.chargeCvv = 922;
+    $scope.chargeExpireMonth = 1;
+    $scope.chargeExpireYear = 2017;
+    $scope.chargeEmail = 'dkontizas@gmail.com';
+    $scope.chargeAddress = '1350 columbia st';
+    $scope.chargeCity = 'San Diego';
+    $scope.chargeState = 'CA';
+    $scope.chargeCountry = 'USA';
+    $scope.chargeZip = '92101';
+    $scope.chargePhone = '6198864467';
+
+
+
+    $scope.authType = 'Charge';
+    
+    // SUBMIT AUTHORIZATION FORM
+    $scope.auth_form = {};
     $scope.submit = function() {
 
         if($scope.auth_form.$valid) {
@@ -85,174 +118,172 @@ $scope.auth_form = {};
         $('.virtual_panel').slideUp(300);
         $('.userSearch').slideDown(300);
 
-} else {
+        } else {
         $('.userError').slideDown(300);
 
         $timeout(function() {
             $('.userError').slideUp(300);
         },1000);
-    }
+        }
+    } // END AUTHORIZATION FORM
 
-        
-    } //end submit
-
-    // submit CAPTURE FORM
-$scope.capture_form = {};
-
+    // SUBMIT CAPTURE FORM
+    $scope.capture_form = {};
     $scope.captureSubmit = function() {
         
-    if($scope.capture_form.$valid) {
+        if($scope.capture_form.$valid) {
 
-        var Query = {
-            "TransactionType":6,
-            "PreviousTransactionNumber":$scope.user.TransNumber,
-            "AuthorizationCode":$scope.user.AuthCode,
-            "ReferenceNumber":$scope.user.RefNumber
-        }
-
-        // POST REQUEST
-        var promise = $http({
-            method:'POST',
-            url: baseUrl + 'vterminal',
-            data:Query
-        });
-
-
-        promise.success(function(data,status) {
-            
-            $scope.payload = data.ApiResponse.ResultCode;
-            $scope.Response = data.ApiResponse;
-            $scope.Request = data.ApiRequest;
-            $scope.RepStatus = data.ApiResponse.ResultCode;
-
-            var Result = data.ApiResponse.ResultCode;
-            // check success or fail of transaction
-            if(Result === 0) {
-                $scope.ResponseTxt = 'Success';
-                $scope.repStatus = 0;
-            } else  {
-                $scope.ResponseTxt = 'Failed';
+            var Query = {
+                "TransactionType":6,
+                "PreviousTransactionNumber":$scope.user.TransNumber,
+                "AuthorizationCode":$scope.user.AuthCode,
+                "ReferenceNumber":$scope.user.RefNumber
             }
-            $scope.reqStatus = 0;
-            $scope.RequestTxt = 'Success';
-           
-            console.log(data);
 
-            $('.comm-panel').slideDown(300);
+            // POST REQUEST
+            var promise = $http({
+                method:'POST',
+                url: baseUrl + 'vterminal',
+                data:Query
+            });
 
-            // copy text
-            $rootScope.copyData = data;
-        
-        }).error(function(data,status) {
+
+            promise.success(function(data,status) {
+                
+                $scope.payload = data.ApiResponse.ResultCode;
+                $scope.Response = data.ApiResponse;
+                $scope.Request = data.ApiRequest;
+                $scope.RepStatus = data.ApiResponse.ResultCode;
+
+                var Result = data.ApiResponse.ResultCode;
+                // check success or fail of transaction
+                if(Result === 0) {
+                    $scope.ResponseTxt = 'Success';
+                    $scope.repStatus = 0;
+                } else  {
+                    $scope.ResponseTxt = 'Failed';
+                }
+                $scope.reqStatus = 0;
+                $scope.RequestTxt = 'Success';
+               
+                console.log(data);
+
+                $('.comm-panel').slideDown(300);
+
+                // copy text
+                $rootScope.copyData = data;
             
-            $scope.reqStatus = 'Failed';
-            $scope.repStatus = 'Failed';
+            }).error(function(data,status) {
+                
+                $scope.reqStatus = 'Failed';
+                $scope.repStatus = 'Failed';
 
-            $('.comm-panel').slideDown(300);
+                $('.comm-panel').slideDown(300);
 
-        });
-        //////////////////
-        
-        $('.virtual_panel').slideUp(300);
-        $('.userSearch').slideDown(300);
-        
-    } else {
+            });
+            //////////////////
+            
+            $('.virtual_panel').slideUp(300);
+            $('.userSearch').slideDown(300);
+            
+        } else {
+          //FORM NOT COMPLETELY FILLED OUT
+          $scope.errorMsg = 'Please ensure to fill in all required fields';
+          $('.errorMsg').slideDown(500);
+          $timeout(function() {
+              $('.errorMsg').slideUp(500);
+          },3000);
 
-        $('.userError').slideDown(300);
+        }
+    } // END CAPTURE FORM
 
-        $timeout(function() {
-            $('.userError').slideUp(300);
-        },1000);
-
-    }
-
-    } // end submit
-
+    // SUBMIT CHARGE FORM
     $scope.charge_form = {};
-    // submit CHARGE FORM
     $scope.chargeSubmit = function() {
 
         if($scope.charge_form.$valid) {
        
-        var Query =  {
-          "TransactionType": 1,
-          "MidGroupId": $scope.user.midGroup,
-          "Amount": +$scope.user.amount,
-          "CurrencyId": $scope.charge_form.currency,
-          "CardNumber": $scope.user.ccNumber,
-          "Cvv": $scope.user.Cvv,
-          "ExpirationMonth": +$scope.user.ExpireMonth,
-          "ExpirationYear": +$scope.user.ExpireYear,
-          "FirstName": $scope.user.fname,
-          "LastName": $scope.user.lname,
-          "Email": $scope.user.email,
-          "Address1": $scope.user.address,
-          "City": $scope.user.city,
-          "State": $scope.user.state,
-          "Country": $scope.user.country,
-          "Zip": $scope.user.zip,
-          "Phone": $scope.user.phone,
-          "ReferenceNumber": $scope.user.refnumber
-        }
-
-        console.log(Query);
-        //$scope.Request = Query;
-       
-        var promise = $http({
-            method:'POST',
-            url: baseUrl + 'vterminal',
-            data:Query
-        });
-
-        promise.success(function(data,status) {
-
-            data.ApiRequest.Cvv = '*' + '*' + '*';
-
-            $scope.payload = data.ApiResponse.ResultCode;
-            $scope.Response = data.ApiResponse;
-            $scope.Request = data.ApiRequest;
-            $scope.RepStatus = data.ApiResponse.ResultCode;
-
-
-            var Result = data.ApiResponse.ResultCode;
-            // check success or fail of transaction
-            if(Result === 0) {
-                $scope.ResponseTxt = 'Success';
-                $scope.repStatus = 0;
-            } else  {
-                $scope.ResponseTxt = 'Failed';
+            var Query =  {
+              "TransactionType": 1,
+              "MidGroupId": $scope.chargeMidGroup,
+              "Amount": +$scope.chargeAmount,
+              "CurrencyId": $scope.chargeCurrency,
+              "CardNumber": $scope.chargeCcNumber,
+              "Cvv": $scope.chargeCvv,
+              "ExpirationMonth": +$scope.chargeExpireMonth,
+              "ExpirationYear": +$scope.chargeExpireYear,
+              "FirstName": $scope.chargeFname,
+              "LastName": $scope.chargeLname,
+              "Email": $scope.chargeEmail,
+              "Address1": $scope.chargeAddress,
+              "City": $scope.chargeCity,
+              "State": $scope.chargeState,
+              "Country": $scope.chargeCountry,
+              "Zip": $scope.chargeZip,
+              "Phone": $scope.chargePhone,
+              "ReferenceNumber": $scope.chargeRefnumber
             }
-            $scope.reqStatus = 0;
-            $scope.RequestTxt = 'Success';
 
-            $('.comm-panel').slideDown(300);
+            console.log(Query);
+               
+            var promise = $http({
+                method:'POST',
+                url: baseUrl + 'vterminal',
+                data:Query
+            });
 
-            // copy text
-            $rootScope.copyData = data;
+            promise.success(function(data,status) {
 
-        }).error(function(data,status) {
-            console.log(status);
+                conole.log(data);
 
-            $scope.reqStatus = 'Failed';
-            $scope.repStatus = 'Failed'
-            $('.comm-panel').slideDown(300);
+                data.ApiRequest.Cvv = '*' + '*' + '*';
+
+                $scope.payload = data.ApiResponse.ResultCode;
+                $scope.Response = data.ApiResponse;
+                $scope.Request = data.ApiRequest;
+                $scope.RepStatus = data.ApiResponse.ResultCode;
 
 
-        });
-       
+                var Result = data.ApiResponse.ResultCode;
+                // check success or fail of transaction
+                if(Result === 0) {
+                    $scope.ResponseTxt = 'Success';
+                    $scope.repStatus = 0;
+                } else  {
+                    $scope.ResponseTxt = 'Failed';
+                }
+                $scope.reqStatus = 0;
+                $scope.RequestTxt = 'Success';
 
-        $('.virtual_panel').slideUp(300);
-        $('.userSearch').slideDown(300);
-        
-    } else {
-        
-        $('.userError').slideDown(300);
+                $('.comm-panel').slideDown(300);
 
-        $timeout(function() {
-            $('.userError').slideUp(300);
-        },1000);
+                // copy text
+                $rootScope.copyData = data;
 
-    }
+            }).error(function(data,status) {
+                console.log(status);
+
+                $scope.reqStatus = 'Failed';
+                $scope.repStatus = 'Failed'
+                $('.comm-panel').slideDown(300);
+
+
+            });
+           
+
+            $('.virtual_panel').slideUp(300);
+            $('.userSearch').slideDown(300);
+            
+        } else {
+            
+          //FORM NOT COMPLETELY FILLED OUT
+          $scope.errorMsg = 'Please ensure to fill in all required fields';
+          $('.errorMsg').slideDown(500);
+          $timeout(function() {
+              $('.errorMsg').slideUp(500);
+          },3000);
+
+        }
      
 
     } // end submit
