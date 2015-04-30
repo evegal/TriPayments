02ba71650068302d49482cbbd5652fc8dcc643de
Demetrios Kontizas <dkontizas@gmail.com>
2015-04-30 16:12:34 -0700

    three reports for recurring payments

--- app/features/src/recurringpayments/subscriptionsReportCtrl.js
+++ app/features/src/recurringpayments/subscriptionsReportCtrl.js
@@ -9,15 +9,12 @@
   
  **  *************************************** **/
 
-app.controller('subscriptionsReportCtrl', function($scope,$http,$timeout,$filter,Notify,baseUrl) {
+app.controller('subscriptionsReportCtrl', function($scope,$http,$timeout,$filter,$state,Notify,baseUrl) {
+    $scope.reportSelect = 0;
 
+    $scope.recurringReportSubmit = function() {
 
-
-
-    $scope.subscriptionReportSubmit = function() {
-
-      var empty = undefined,
-          queryStartDate = $scope.subForm.startDate || new Date(),
+      var queryStartDate = $scope.subForm.startDate || new Date(),
           queryEndDate = $scope.subForm.endDate || new Date(),
           datefilter = $filter('date'),
           startDateFormat = datefilter(queryStartDate,'MM/dd/yyyy'),
@@ -30,32 +27,104 @@ app.controller('subscriptionsReportCtrl', function($scope,$http,$timeout,$filter
               "EndDate":endDateFormat
             }
 
-            $http({
-                method:'POST',
-                url:baseUrl + 'recurring/report/queuedattempts',
-                data:Query
-            }).success(function(data,status) {
-                // FOR DISPLAY IN COMMUNCATION DETAILS
+          if ($scope.reportSelect == 0){
 
+              $http({
+                  method:'POST',
+                  url:baseUrl + 'recurring/report/queuedattempts',
+                  data:Query
+              }).success(function(data,status) {
+                // FOR DISPLAY IN VIEW
+                $scope.queuedReportBulk = data;
+                $scope.queuedReportAmount = data.length;
+                $scope.shownQueuedReport = $scope.queuedReportBulk;
 
-            }).error(function(data,status) {
-                // FOR DISPLAY IN COMMUNCATION DETAILS
+                $('.report_results_qued').slideDown(300);
+                $('.report_parameters').slideUp(300);
 
+                $('.report_results_subscription').hide();
+                $('.report_results_subscriber').hide();
 
-            });
-
+              }).error(function(data,status) {
+                  //FORM NOT COMPLETELY FILLED OUT
+                  $scope.errorMsg = 'There is an error with the API please contact your customer support. Error Code: ' + status;
+                  $('.errorMsg').slideDown(500);
+                  $timeout(function() {
+                      $('.errorMsg').slideUp(500);
+                  },3000);
+              });
            
-        } else {
-            
-          //FORM NOT COMPLETELY FILLED OUT
-          $scope.errorMsg = 'Please ensure to fill in all required fields';
-          $('.errorMsg').slideDown(500);
-          $timeout(function() {
-              $('.errorMsg').slideUp(500);
-          },3000);
+          } else if ($scope.reportSelect == 1) {
+
+              $http({
+                  method:'POST',
+                  url:baseUrl + 'recurring/subscribers/summary',
+                  data:Query
+              }).success(function(data,status) {
+                // FOR DISPLAY IN VIEW
+                $scope.subscriberReportBulk = data;
+                $scope.subscriberReportAmount = data.length;
+                $scope.shownsubscriberReport = $scope.subscriberReportBulk;
+
+                $('.report_results_subscriber').slideDown(300);
+                $('.report_parameters').slideUp(300);
+
+                $('.report_results_subscription').hide();
+                $('.report_results_qued').hide();
+
+              }).error(function(data,status) {
+                  //FORM NOT COMPLETELY FILLED OUT
+                  $scope.errorMsg = 'There is an error with the API please contact your customer support. Error Code: ' + status;
+                  $('.errorMsg').slideDown(500);
+                  $timeout(function() {
+                      $('.errorMsg').slideUp(500);
+                  },3000);
+              });
+          
+          } else {
+
+              $http({
+                  method:'POST',
+                  url:baseUrl + 'recurring/subscriptions/summary',
+                  data:Query
+              }).success(function(data,status) {
+                // FOR DISPLAY IN VIEW
+                $scope.subscriptionReportBulk = data;
+                $scope.subscriptionReportAmount = data.length;
+                $scope.shownsubscriptionReport = $scope.subscriptionReportBulk;
+
+                $('.report_results_subscription').slideDown(300);
+                $('.report_parameters').slideUp(300);
+
+                $('.report_results_subscriber').hide();
+                $('.report_results_qued').hide();
+
+
+              }).error(function(data,status) {
+                  //FORM NOT COMPLETELY FILLED OUT
+                  $scope.errorMsg = 'There is an error with the API please contact your customer support. Error Code: ' + status;
+                  $('.errorMsg').slideDown(500);
+                  $timeout(function() {
+                      $('.errorMsg').slideUp(500);
+                  },3000);
+              });
+
+          }
 
         }
-    } // end submit
+
+    } // end recurringReportSubmit
+
+
+    // NEW SEARCH
+    $scope.new_search = function() {
+      $state.go($state.$current, null, { reload: true });     
+    };
+
+    //MODIFY SEARCH
+    $scope.modify_search = function(){
+      $('.report_parameters').slideDown(300);      
+    }
 
 
 });
